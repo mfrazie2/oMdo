b192e5ed4a56427e9b5f72cb62900640682a3707
chrislpatton <chrislpatton@gmail.com>
2016-03-12 19:11:18 -0500

    (feat) auth components and containers routes

--- client/app/components/auth.js
+++ client/app/components/auth.js
@@ -1,15 +1,18 @@
 var React = require('react');
-// TODO import styles
+var PropTypes = require('react').PropTypes;
+{/*TODO import styles*/}
 var Auth = function(props) {
   return (
     <div>
       <form onSubmit={props.onSubmit}>
         <input
           type="text"
+          onChange={props.onUpdateUsername}
           value={props.username}
           placeholder="username" />
         <input
           type="text"
+          onChange={props.onUpdatePassword}
           value={props.password}
           placeholder="password"/>
         <button type="submit">{props.text}</button>
@@ -18,4 +21,12 @@ var Auth = function(props) {
   )
 };
 
+Auth.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  onUpdateUsername: PropTypes.func.isRequired,
+  onUpdatePassword: PropTypes.func.isRequired,
+  text: PropTypes.string,
+  username: PropTypes.string,
+  password: PropTypes.string
+};
 module.exports = Auth;
