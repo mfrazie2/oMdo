9eabd9d5385c416cc203288b5e72e52a7d0b5ae8
Christopher Patton <christopherpatton@Christophers-iMac.local>
2016-03-12 19:11:18 -0500

    (feat) auth components and containers routes

---
+++ client/app/containers/authContainer.js
@@ -0,0 +1,53 @@
+var React = require('react');
+var Auth = require('../components/auth');
+
+var AuthContainer = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+  getInitialState: function() {
+    var text;
+    if (this.props.location.pathname === '/signin') {
+      text = 'Sign in';
+    }
+    if (this.props.location.pathname === '/signup') {
+      text = 'Sign up';
+    }
+    return {
+      username: '',
+      password: '',
+      buttonText: text || 'Submit'
+    };
+  },
+  handleSubmit: function(event) {
+    event.preventDefault();
+    this.context.router.push({
+      pathname: '/home',
+      state: {
+        username: this.state.username,
+      }
+    });
+  },
+  handleUpdateUsername: function(event) {
+    this.setState({
+      username: event.target.value
+    });
+  },
+  handleUpdatePassword: function(event) {
+    this.setState({
+      password: event.target.value
+    })
+  },
+  render: function() {
+    return (
+      <Auth onSubmit={this.handleSubmit}
+        onUpdateUsername={this.handleUpdateUsername}
+        onUpdatePassword={this.handleUpdatePassword}
+        username={this.state.username}
+        password={this.state.password}
+        text={this.state.buttonText} />
+    )
+  }
+});
+
+module.exports = AuthContainer;
