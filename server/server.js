dbeb311493c4923e8677945fb13cf32b32b7e6ba
sanfordcb <sanford0518@gmail.com>
2016-03-14 16:18:54 -0500

    Add conditional statement to server file for production environment

--- server/server.js
+++ server/server.js
@@ -14,29 +14,42 @@ var app = express();
 // middleware(app, express);
 middleware(app);
 
-var port = process.env.PORT || 8080;
+var isProduction = process.env.NODE_ENV === 'production';
+var port = isProduction ? process.env.port : 8080;
 
 app.use('/api', routes);
 
-// Configure webpack dev middleware for use with server
-var webpackMiddleWareConfig = webpackDevMiddleware(compiler, {
-  publicPath: config.output.publicPath,
-  stats: {colors: true}
-});
 
-// Connect webpack dev middleware to server
-app.use(webpackMiddleWareConfig);
+if(!isProduction) {
+  
+  // Configure webpack dev middleware for use with server
+  var webpackMiddleWareConfig = webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    stats: {colors: true}
+  });
 
-// Connect webpack hot middleware to server
-app.use(webpackHotMiddleware(compiler, {
-  log: console.log
-}))
+  // Connect webpack dev middleware to server
+  app.use(webpackMiddleWareConfig);
+
+  // Connect webpack hot middleware to server
+  app.use(webpackHotMiddleware(compiler, {
+    log: console.log
+  }))
 
 
-app.get('*', function response(req, res) {
-  res.write(webpackMiddleWareConfig.fileSystem.readFileSync(path.join(__dirname, '../dist/index.html')));
-  res.end();
-});
+  app.get('*', function response(req, res) {
+    res.write(webpackMiddleWareConfig.fileSystem.readFileSync(path.join(__dirname, '../dist/index.html')));
+    res.end();
+  });
+
+} else {
+  app.use(express.static(path.join(__dirname, '..dist/index.html')));
+
+  app.get('*', function response(req, res) {
+    res.sendFile(path.join(__dirname, '..dist/index.html'));
+  })
+}
+
 
 app.listen(port, console.log.bind(console, 'Listening on port', port));
 
