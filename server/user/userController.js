742227c77fad5cf7b52220bf865ed705987fe64d
sanfordcb <sanford0518@gmail.com>
2016-03-13 22:43:49 -0500

    Add initial controllers and routes for testing

---
+++ server/user/userController.js
@@ -0,0 +1,8 @@
+module.exports = {
+  dataTest: function(req, res) {
+    res.send('Data about your feelings!');
+  },
+  diaryTest: function(req, res) {
+    res.send('A diary of your feelings!');
+  }
+}
