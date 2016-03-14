c32c79ab98990b94d8da3007244216bb6a977b25
sanfordcb <sanford0518@gmail.com>
2016-03-14 18:47:38 -0500

    Refactor server, middleware, and routes for modularization

--- server/routes.js
+++ server/routes.js
@@ -4,10 +4,8 @@ var authRoutes = require('./auth/authRoutes');
 var surveyRoutes = require('./survey/surveyRoutes');
 var userRoutes = require('./user/userRoutes');
 
-var app = express.Router();
-
-app.use('/auth', authRoutes);
-app.use('/survey', surveyRoutes);
-app.use('/user', userRoutes);
-
-module.exports = app;
+module.exports = function(app) {
+  app.use('/auth', authRoutes);
+  app.use('/survey', surveyRoutes);
+  app.use('/user', userRoutes);
+};
--- server/server.js
+++ server/server.js
@@ -2,30 +2,15 @@ var express = require('express');
 var path = require('path');
 var middleware = require('./middleware');
 require('dotenv').config();
-var devMiddleware = require('./devMiddleware');
-var routes = require('./routes');
 
 var app = express();
-middleware(app);
 
-var isProduction = process.env.NODE_ENV === 'production';
-var port = isProduction ? process.env.port : 8080;
+var port = middleware.isProduction() ? process.env.port : 8080;
 
-app.use('/api', routes);
-
-if(!isProduction) {
-  devMiddleware(app);
-
-  app.get('*', function response(req, res) {
-    res.write(webpackMiddleWareConfig.fileSystem.readFileSync(path.join(__dirname, '../dist/index.html')));
-    res.end();
-  });
+if(!middleware.isProduction()) {
+  middleware.devMiddleware(app);
 } else {
-  app.use(express.static(path.join(__dirname, '..dist/index.html')));
-
-  app.get('*', function response(req, res) {
-    res.sendFile(path.join(__dirname, '..dist/index.html'));
-  })
+  middleware.prodMiddleware(app, express);
 }
 
 app.listen(port, console.log.bind(console, 'Listening on port', port));
