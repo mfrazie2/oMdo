9d967a85dc07174ab3bc9fc0b3fac67b4ba0cb12
sanfordcb <sanford0518@gmail.com>
2016-03-14 16:37:07 -0500

    Cleanup

--- server/middleware.js
+++ server/middleware.js
@@ -1,16 +1,6 @@
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 
-// Commented out main routes for now
-// Refactor when we implement API requests
-
-// var mainRoutes = require('./main/mainRoutes');
-
-// module.exports = function (app, express) {
-//   app.use(morgan('dev'));
-//   mainRoutes(app, express);
-// };
-
 module.exports = function(app) {
   app.use(bodyParser.urlencoded({
     extended: true
