5b478356212c8f9b59ef268bc17021d78d86755a
sanfordcb <sanford0518@gmail.com>
2016-03-14 16:37:07 -0500

    Cleanup

--- server/main/mainController.js
+++
@@ -1,9 +0,0 @@
-// module.exports = {
-//   helloWorld: function(req, res) {
-//     res.send('Hello Birdcage & Sleet');
-//   },
-//   errorHandler: function(error) {
-//     console.log('There was an error in main:', error);
-//     res.status(500).send();
-//   }
-// };
