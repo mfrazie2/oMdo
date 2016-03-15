a797594b20296ace6d47031c6e7be7c4c714bdfb
John Soo <jsoo1@asu.edu>
2016-03-15 11:31:41 -0700

    (fix)Fixes remnants of rebase, server not using express.Router

--- server/middleware.js
+++ server/middleware.js
@@ -10,22 +10,22 @@ exports.configure = function(app, express) {
   }));
   app.use(bodyParser.json());
   app.use(morgan('dev'));
-  app.use('/api', routes);
+  routes(app, express);
 
   if(!this.isProduction()) {
     app.use(webpackMiddleware.devConfig);
     app.use(webpackMiddleware.hotConfig);
     app.get('*', function response(req, res) {
-      res.write(webpackMiddleWare.devConfig.fileSystem.readFileSync(path.join(__dirname, '../dist/index.html')));
+      res.write(webpackMiddleware.devConfig.fileSystem.readFileSync(path.join(__dirname, '../dist/index.html')));
       res.end();
     });
   } else {
     app.use(express.static(path.join(__dirname, '..dist/index.html')));
     app.get('*', function response(req, res) {
       res.sendFile(path.join(__dirname, '..dist/index.html'));
-    });  
-  } 
-}; 
+    });
+  }
+};
 
 exports.isProduction = function() {
   return process.env.NODE_ENV === 'production';
