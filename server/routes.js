531f7fc6cbb4e6a8634cef800006b9f083b89575
John Soo <jsoo1@asu.edu>
2016-03-15 11:31:41 -0700

    (fix)Fixes remnants of rebase, server not using express.Router

--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,8 @@
-var express = require('express');
-
 var authRoutes = require('./auth/authRoutes');
 var surveyRoutes = require('./survey/surveyRoutes');
 var userRoutes = require('./user/userRoutes');
 
-module.exports = function(app) {
+module.exports = function(app, express) {
   app.use('/auth', authRoutes);
   app.use('/survey', surveyRoutes);
   app.use('/user', userRoutes);
