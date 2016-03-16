ca2a4c955e1fda41565f5259ce39662d2e3a3c16
Christopher Patton <christopherpatton@Christophers-iMac.local>
2016-03-16 19:23:10 -0400

    (feat) db user mongoose schema

---
+++ server/db/models/userSchema.js
@@ -0,0 +1,63 @@
+var mongoose = require('mongoose');
+var Q = require('q');
+var bcrypt = require('bcrypt-node.js');
+var SALT_WORK_FACTOR = 10;
+
+var UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  survey:[{type: Schema.Types.ObjectId, ref: 'Survey'}],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  salt: String
+});
+
+
+// resources http://devsmash.com/blog/password-authentication-with-mongoose-and-bcrypt
+// https://github.com/kriskowal/q
+
+UserSchema.pre('save', function() {
+  var user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
+    bycrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      user.salt = salt;
+      next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function(passwordAttempt) {
+  var defer = Q.defer();
+  var hashed = this.password;
+  bycrypt.compare(passwordAttemp, hashed, function(err, isMatched) {
+    if (err) {
+      defer.reject(err);
+    } else {
+      defer.resolve(isMatched);
+    }
+  });
+  return defer.promise;
+};
+
+var User = mongoose.model('User', UserSchema);
+
+module.exports = User;
