5adc83360ebb4f552dbe0769af8e4fadf97fa38f
sanfordcb <sanford0518@gmail.com>
2016-03-16 13:55:06 -0500

    Initial redux implementation attempt

--- client/app/actions/actions.js
+++ client/app/actions/actions.js
@@ -1,19 +1,60 @@
+var axios = require('axios');
+
 //action types
-exports.SIGN_IN = 'SIGN_IN';
-exports.SIGN_UP = 'SIGN_UP';
-exports.SUBMIT_SURVEY = 'SUBMIT_SURVEY';
+exports.SIGN_IN_SUBMIT = 'SIGN_IN_SUBMIT';
+exports.SIGN_IN_SUCCESS = 'SIGN_IN_SUCCESS';
+exports.SIGN_IN_FAILURE = 'SIGN_IN_FAILURE';
+
+exports.SIGN_UP_SUBMIT = 'SIGN_UP_SUBMIT';
+exports.SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
+exports.SIGN_UP_FAILURE = 'SIGN_UP_FAILURE';
+
+exports.SURVEY_SUBMIT = 'SURVEY_SUBMIT';
+exports.SURVEY_SUCCESS = 'SURVEY_SUCCESS';
+exports.SURVEY_FAILURE = 'SURVEY_FAILURE';
 
 //action-creators
-exports.signIn = function(username, password) {
-  return {type: 'SIGN_IN', username, password};
+exports.signInSubmit = function() {
+  return {type: 'SIGN_IN_SUBMIT'};
+};
+exports.signInSuccess = function(user) {
+  return {type: 'SIGN_IN_SUCCESS', user};
+};
+exports.signInFailure = function(err) {
+  return {type: 'SIGN_IN_FAILURE', err};
 };
 
-exports.signUp = function(signupForm) {
-  return {type: 'SIGN_UP', signupForm};
+
+exports.signUpSubmit = function(signupForm) {
+  return {type: 'SIGN_UP_SUBMIT', signupForm};
+};
+exports.signUpSuccess = function(signupForm) {
+  return {type: 'SIGN_UP_SUCCESS', signupForm};
+};
+exports.signUpFailure = function(signupForm) {
+  return {type: 'SIGN_UP_FAILURE', signupForm};
 };
 
-exports.submitSurvey = function(surveyForm) {
-  return {type: 'SUBMIT_SURVEY', surveyForm};
+
+exports.surveySubmit = function(surveyForm) {
+  return {type: 'SURVEY_SUBMIT', surveyForm};
+};
+exports.surveySuccess = function(surveyForm) {
+  return {type: 'SURVEY_SUCCESS', surveyForm};
+};
+exports.surveyFailure = function(surveyForm) {
+  return {type: 'SURVEY_FAILURE', surveyForm};
 };
 
-
+exports.signIn = function() {
+  return function(dispatch) {
+    dispatch(signInSubmit());
+    axios.get('/auth')
+    .then(function(response) {
+      dispatch(signInSuccess(response.data));
+    })
+    .catch(function(response) {
+      dispatch(signInFailure(response.data));
+    });
+  }
+}
--- client/app/index.js
+++ client/app/index.js
@@ -1,9 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var routes = require('./config/routes');
+
+// var routes = require('./config/routes');
 var css = require('./styles/main.css');
 
+var Root = require('./config/routes');
+
 ReactDOM.render(
-  routes,
+  <Root />,
   document.getElementById('app')
 )
