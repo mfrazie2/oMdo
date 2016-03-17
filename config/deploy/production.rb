796992e0cfe3624d27890e7c77488dfefec33447
John Soo <jsoo1@asu.edu>
2016-03-16 21:22:32 -0700

    (fix)Used ssh url instead of http in deploy.rb and production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 server 'ec2-54-213-147-55.us-west-2.compute.amazonaws.com', user: 'ec2-user', roles: %w{app}
 set :branch, 'staging'
 set :application, 'skynet'
-set :repo_url, 'git@github.com/BirdcageSleet/skynet.git'
+set :repo_url, 'git@github.com:BirdcageSleet/skynet.git'
 set :deploy_to, '/var/www/skynet'
 set :ssh_options, { :forward_agent => true}
 
