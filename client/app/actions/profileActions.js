81f4ba243314d91013d7fed77fdcc754db7dcf48
sanfordcb <sanford0518@gmail.com>
2016-03-17 12:49:14 -0500

    Presentation - display all questions, remove text decoration, comment out buttons

--- client/app/actions/profileActions.js
+++ client/app/actions/profileActions.js
@@ -1,6 +1,6 @@
-exports.CONSOLE_LOG_SOMETHING = 'CONSOLE_LOG_SOMETHING';
+// exports.CONSOLE_LOG_SOMETHING = 'CONSOLE_LOG_SOMETHING';
 
-exports.consoleLogSomething = function() {
-  return {type: 'CONSOLE_LOG_SOMETHING'};
-};
+// exports.consoleLogSomething = function() {
+//   return {type: 'CONSOLE_LOG_SOMETHING'};
+// };
 
--- client/app/components/question.js
+++ client/app/components/question.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
 var Link = require('react-router').Link;
 var styleable = require('react-styleable').default
 var css = require('../styles/question.css');
@@ -7,17 +8,19 @@ console.log(typeof styleable)
 
 var currentIndex = currentIndex || 0;
 
-function Question(props) {
-  console.log(props);
-  return(
-    <div className={props.css.root}>
-      <button className={props.css.prev} onClick={props.onPrev}>last question</button>
-      <span className={props.css.currentQuestion}>Question</span>
-      <button className={props.css.next} onClick={props.onNext}>next question</button>
-    </div>
-  )
-}
 
+
+
+// function Question(props) {
+//   console.log(props);
+//   return(
+//     <div className={props.css.root}>
+//       <button className={props.css.prev} onClick={props.onPrev}>last question</button>
+//       <span className={props.css.currentQuestion}>Question</span>
+//       <button className={props.css.next} onClick={props.onNext}>next question</button>
+//     </div>
+//   )
+// }
 
 // function getChild(props, currentIndex) {
 //   return React.Children.map(props.children, function(element, i){
@@ -27,8 +30,63 @@ function Question(props) {
 //   })
 // }
 
+
+
 // function Question(props) {
-//   console.log(props);
+//   console.log('Question props ', props);
+//   return(
+//     <div className={props.css.root}>
+//       <button className={props.css.prev} onClick={props.prevQuestion}>last question</button>
+//       <span className={props.css.currentQuestion}>{getChild(props, currentIndex)}</span>
+//       <button className={props.css.next} onClick={props.nextQuestion}>next question</button>
+//     </div>
+//   )
+// }
+
+// Question.propTypes = {
+//   prevQuestion: PropTypes.func.required,
+//   getChild: PropTypes.func.required,
+//   nextQuestion: PropTypes.func.required
+// }
+
+// function getChild(props, currentIndex) {
+//   return React.Children.map(props.children, function(element, i){
+//     console.log(currentIndex);
+//     if (currentIndex === i){
+//       return element;
+//     }
+//   })
+// }
+
+function getChild(props, currentIndex) {
+  return props.children[currentIndex];
+}
+
+function prevChild(props, currentIndex) {
+  return props.children[currentIndex-1];
+}
+
+function nextChild(props, currentIndex) {
+  return props.children[currentIndex+1];
+}
+
+function testFunc() {
+  console.log('What is happening here?');
+}
+
+function Question(props) {
+  console.log('Question ', props);
+  return(
+    <div className={props.css.root}>
+      {/*<button className={props.css.prev} onClick={testFunc()}>last question</button>*/}
+      <span className={props.css.currentQuestion}>{props.children}</span>
+      {/*<button className={props.css.next} onClick={currentIndex+1}>next question</button>*/}
+    </div>
+  )
+}
+
+
+// function Question(props) {
 //   return(
 //     <div className={props.css.root}>
 //       <button className={props.css.prev} onClick={getChild(props, currentIndex-1)}>last question</button>
