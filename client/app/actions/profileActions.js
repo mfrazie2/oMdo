b7dd8893a07d5cc113f1d38c54039a913fdfe4f2
sanfordcb <sanford0518@gmail.com>
2016-03-17 07:54:18 -0500

    Further attempts at implementing Redux

--- client/app/actions/profileActions.js
+++ client/app/actions/profileActions.js
@@ -1,6 +1,8 @@
-// exports.CONSOLE_LOG_SOMETHING = 'CONSOLE_LOG_SOMETHING';
 
-// exports.consoleLogSomething = function() {
-//   return {type: 'CONSOLE_LOG_SOMETHING'};
-// };
+var CONSOLE_LOG_SOMETHING = require('../actions/actions').CONSOLE_LOG_SOMETHING;
+
+exports.consoleLogSomething = function() {
+  return {type: CONSOLE_LOG_SOMETHING};
+};
+
 
--- client/app/reducers/profileReducers.js
+++ client/app/reducers/profileReducers.js
@@ -1,24 +1,25 @@
-// var actions = require('../actions/actions');
+var CONSOLE_LOG_SOMETHING = require('../actions/actions').CONSOLE_LOG_SOMETHING;
 
-// var initialState = {
-//   isLoading: false,
-//   data: [],
-//   error: false,
-//   currentIndex: 0,
-//   reduxResult: 'This is not working!'
-// }
 
-// exports.profileReducer = function(state, action) {
-//   if(state === undefined) {
-//     state = initialState;
-//   }
-//   console.log('action.type ', action);
-//   switch(action.type) {
-//     case CONSOLE_LOG_SOMETHING:
-//       return Object.assign({}, state, {reduxResult: 'Did this work?'});
-//     default:
-//       console.log('Default is working');
-//       console.log('Initial state, ' + state.reduxResult);
-//       return state;
-//   }
-// }
+var initialState = {
+  isLoading: false,
+  data: [],
+  error: false,
+  currentIndex: 0,
+  reduxResult: 'This is not working!'
+}
+
+exports.profileReducer = function(state, action) {
+  if(state === undefined) {
+    state = initialState;
+  }
+  console.log('action.type ', action);
+  switch(action.type) {
+    case CONSOLE_LOG_SOMETHING:
+      return Object.assign({}, state, {reduxResult: 'Did this work?'});
+    default:
+      console.log('Default is working');
+      console.log('Initial state, ' + state.reduxResult);
+      return state;
+  }
+}
