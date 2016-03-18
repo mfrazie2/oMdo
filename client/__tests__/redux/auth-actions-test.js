9ac02d22f7515177a4fd50fb0d7243a2a89a7e43
John Soo <jsoo1@asu.edu>
2016-03-18 13:10:53 -0700

    (test)Adds auth-action-test

---
+++ client/__tests__/redux/auth-actions-test.js
@@ -0,0 +1,45 @@
+var expect = require('chai').expect;
+var actions = require('../app/actions/authActions');
+var nock = require('nock');
+var thunk = require('redux-thunk');
+var configureMockStore = require('redux-mock-store').configureMockStore
+​
+describe('sign up', function(){
+  it('should have a function signUpSubmit that returns an action type SIGN_UP_SUBMIT', function(){
+    var expectedAction = {type: 'SIGN_UP_SUBMIT', signupForm};
+    expect(actions.signUpSubmit).to.be.a.function;
+    expect(actions.signUpSubmit()).to.equal(expectedAction);
+  });
+  it('should have a function signUpSucess that returns an action type SIGN_UP_SUCCESS', function(){
+    var expectedAction = {type: 'SIGN_UP_SUCCESS', signupForm};
+    expect(actions.signUpSuccess).to.be.a.function;
+    expect(actions.signUpSuccess()).to.equal(expectedAction);
+  });
+  it('should have a function signUpFailure that returns an action type SIGN_UP_FAILURE', function(){
+    var expectedAction = {type: 'SIGN_UP_FAILURE', signupForm};
+    expect(actions.signUpFailure).to.be.a.function;
+    expect(actions.signUpFailure()).to.equal(expectedAction);
+  });
+});
+​
+describe('sign in', function(){
+  it('should have a function signInSubmit that returns an action type SIGN_IN_SUBMIT', function(){
+    var expectedAction = {type: 'SIGN_IN_SUBMIT', signinForm};
+    expect(actions.signInSubmit).to.be.a.function;
+    expect(actions.signInSubmit()).to.equal(expectedAction);
+  });
+  it('should have a function signUpSucess that returns an action type SIGN_IN_SUCCESS', function(){
+    var expectedAction = {type: 'SIGN_IN_SUCCESS', signinForm};
+    expect(actions.signInSuccess).to.be.a.function;
+    expect(actions.signInSuccess()).to.equal(expectedAction);
+  });
+  it('should have a function signUpFailure that returns an action type SIGN_IN_FAILURE', function(){
+    var expectedAction = {type: 'SIGN_IN_FAILURE', signinForm};
+    expect(actions.signInFailure).to.be.a.function;
+    expect(actions.signInFailure()).to.equal(expectedAction);
+  });
+});
+​
+describe('auth axios calls', function(){
+  var middleware =
+})
