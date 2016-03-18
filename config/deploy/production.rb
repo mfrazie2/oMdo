d5290b7718db797c13c9921bc8b9c7e576e99b39
John Soo <jsoo1@asu.edu>
2016-03-18 12:43:21 -0700

    (fix)Changes watched branch to master

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,8 @@
-set :branch, 'staging'
+set :branch, 'master'
 set :application, 'skynet'
 set :repo_url, 'git@github.com:BirdcageSleet/skynet.git'
 set :deploy_to, '/var/www/skynet'
-set :ssh_options, { :forward_agent => true}
+set :ssh_options, { :forward_agent => true }
 set :stage, :production
 
 server 'ec2-54-213-147-55.us-west-2.compute.amazonaws.com', user: 'ec2-user', roles: %w{web app}
