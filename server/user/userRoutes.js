edae0cd919e22884da81ebaac18f61b5eefc7685
chrislpatton <chrislpatton@gmail.com>
2016-03-17 22:05:56 -0400

    (feat) added routes for signin signup and checkAuth

--- server/user/userRoutes.js
+++ server/user/userRoutes.js
@@ -5,5 +5,8 @@ var app = express.Router();
 
 app.get('/data', userController.dataTest);
 app.get('/diary', userController.diaryTest);
+app.post('/signIn',userController.signIn);
+app.post('/signUp', userController.signUp);
+app.get('/signedIn', userController.checkAuth;)
 
 module.exports = app;
