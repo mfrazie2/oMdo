b6ab911550e7ec427c8973f69450fa426105a758
John Soo <jsoo1@asu.edu>
2016-03-22 14:10:30 -0700

    (feat)Adds charts to diary entries

--- client/app/components/diaryEntry.js
+++ client/app/components/diaryEntry.js
@@ -1,4 +1,7 @@
 var React = require('react');
+var Radar = require('react-chartjs').Radar;
+var styleable = require('react-styleable').default;
+var css = require('../styles/question.css');
 
 var puke = function(data) {
     return (
@@ -8,12 +11,45 @@ var puke = function(data) {
     )
 }
 
+var irrelevantFields = {
+  'surveyId': true,
+  '__v': true,
+  '_id': true,
+  'createdOn': true,
+  'createdBy': true,
+  'eventElaborate': true,
+  'moodElaborate': true,
+  'sleepElaborate': true
+};
+
 function Entry (props) {
+  var relevantLabels = Object.keys(props.data).filter(function(field) {
+    return !irrelevantFields[field];
+  });
+
+  var radarData = {
+    labels: relevantLabels,
+    datasets: [
+      {
+        label: "My First dataset",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: relevantLabels.map(function(field){
+          return props.data[field];
+        })
+      }
+    ]
+  }
   return (
-    <li key={props.key}>
+    <li key={props.key} className={props.css.root}>
+      <Radar data={radarData} width='100%' height='100%' />
       {puke(props.data)}
     </li>
   )
 }
 
-module.exports = Entry;
+module.exports = styleable(css)(Entry);
