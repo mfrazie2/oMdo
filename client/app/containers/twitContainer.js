f06a9dfb4a0fe4f19fb76ba395abe6b406b1329f
Mike Frazier <michael.s.frazier@gmail.com>
2016-03-23 22:03:52 -0500

    (feat) Twit components are not ready for app

---
+++ client/app/containers/twitContainer.js
@@ -0,0 +1,16 @@
+// var React = require('react');
+// var Twit = require('../components/twit');
+// var connect = require('react-redux').connect;
+// var twitActions = require('../actions/twitActions');
+// var homeActions = require('../actions/homeActions');
+// var bindActionCreators = require('redux').bindActionCreators;
+
+// var TwitContainer = React.createClass({
+  
+//   handleTwitterChange: function(e) {
+//     e.preventDefault();
+//     var twitterHandle = e.target.value;
+//     this.props.actions.twitterChange(twitterHandle);
+//   }
+  
+// });
