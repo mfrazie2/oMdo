8c68b238e02e0536c2c7428298b87815411b9577
John Soo <jsoo1@asu.edu>
2016-03-24 14:44:32 -0700

    (fix)Fixes profile not loading bug

--- client/app/components/tweetEntry.js
+++ client/app/components/tweetEntry.js
@@ -3,7 +3,6 @@ var styleable = require('react-styleable').default;
 var css = require('../styles/question.css');
 
 function TweetEntry(props) {
-  console.log(props.key, ' ', props.data);
   return (
     <li key={props.key} className={props.css.root}>
       {props.data}
