1735aa2eb43b5541f07ef6780f2a4b723e65d975
Christopher Patton <christopherpatton@Christophers-iMac.local>
2016-03-25 13:45:27 -0400

    (feat) updated radar to display the last three survey entries

--- client/app/components/diary.js
+++ client/app/components/diary.js
@@ -1,19 +1,14 @@
 var React = require('react');
-var Entry = require('./diaryEntry');
+var DiaryEntry = require('./diaryEntry');
 var styleable = require('react-styleable').default;
 var css = require('../styles/profile.css');
 
 function Diary (props) {
   return (
     <ol className={props.css.diary}>
-      {props.surveys.map(function(survey, i) {
-        return (
-          <Entry
-            data={survey}
-            key={i}
-          />
-        )
-      })}
+        <DiaryEntry
+        data={props.surveys}
+      />
     </ol>
   )
 }
