24933a4369fdf7100e37541cb810ce9d73a3d6df
John Soo <jsoo1@asu.edu>
2016-03-25 14:06:04 -0700

    (refactor)Finishes tile refactor. Changes some styling

--- client/app/components/survey.js
+++ client/app/components/survey.js
@@ -2,7 +2,6 @@ var React = require('react');
 var Form = require('./form');
 var Loading = require('../components/loading');
 var Questions = require('./questions');
-var styleable = require('react-styleable').default;
 
 function Survey(props) {
   var key = -1;
