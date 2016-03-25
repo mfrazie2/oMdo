af78b3662a94a046413521ae09b58809e65f344a
John Soo <jsoo1@asu.edu>
2016-03-25 11:04:18 -0700

    (fix)Fixes auth form styling

--- client/app/components/form.js
+++ client/app/components/form.js
@@ -5,8 +5,10 @@ var css = require('../styles/main.css');
 function Form (props) {
   return (
     <div className={props.css.form}>
-      {props.children}
-      <button onClick={props.onSubmit}>{props.text}</button>
+      <div className={props.className}>
+        {props.children}
+        <button onClick={props.onSubmit}>{props.text}</button>
+      </div>
     </div>
   )
 };
