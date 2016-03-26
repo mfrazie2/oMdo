bfec528a03a95da5f77c76e4a6587d736286529b
Christopher Patton <christopherpatton@Christophers-iMac.local>
2016-03-26 18:46:12 -0400

    (feat) added dates to radar chart hover legend

--- client/app/utilities/formatEntryRadarData.js
+++ client/app/utilities/formatEntryRadarData.js
@@ -10,12 +10,12 @@ module.exports = function(data){
     'majorEvent': true,
     'sleepElaborate': true
   };
-
   var relevantLabels = Object.keys(data[0]).filter(function(field) {
     return !irrelevantFields[field];
   });
 
   var dataPlots = [];
+  var dates = [];
   var i = data.length-1;
   var setsToGo  = (data.length >= 7) ? 7 : data.length;
 
@@ -24,6 +24,7 @@ module.exports = function(data){
     for (var k = 0; k < relevantLabels.length; k++) {
       var field = relevantLabels[k];
       dataPlots[i].push(data[i][field] * 10);
+      dates.push(data[i].createdOn.substr(0,10));
     }
     setsToGo -= 1;
     i -= 1;
@@ -32,7 +33,7 @@ module.exports = function(data){
   var fillColors = ['rgba(102,0,204,0.4)', 'rgba(255,0,0,0.4)', 'rgba(255,153,51,0.4)','rgba(255,255,0,0.4)','rgba(0,204,0,0.4)','rgba(51,153,255,0.4)','rgba(0,0,255,0.4)'];
   for (var j = 0; j < dataPlots.length; j++) {
     radarPlots.push({
-      label: 'User Data',
+      label: dates[j],
       strokeColor: fillColors[j],
       pointColor: fillColors[j],
       pointStrokeColor: '#fff',
