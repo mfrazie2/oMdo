b4c80f8d16de3b8a2585e9162d8fc6f9d01ac802
Mike Frazier <michael.s.frazier@gmail.com>
2016-03-27 20:01:44 -0500

    (fix) Change labels on radar graph, switch value order for anxiety question, add labels to ends of slider questions

--- client/app/components/survey/sliderQuestion.js
+++ client/app/components/survey/sliderQuestion.js
@@ -5,7 +5,7 @@ function SliderQuestion (props) {
   return (
     <Tile key={props.key}>
       <p>{props.text} <br/><em><small>(On a scale from {props.minText} to {props.maxText})</small></em></p>
-      <input type='range' min='1' max='5' step='.01' tabIndex={props.tabIndex} onChange={props.handleChange} />
+      {props.minText}<input type='range' min='1' max='5' step='1' tabIndex={props.tabIndex} onChange={props.handleChange} />{props.maxText}
       <br />
     </Tile>
   )
