a0ed05e2e4b6c91839e58b7c8ed2510fa798577a
Mike Frazier <michael.s.frazier@gmail.com>
2016-03-28 17:09:27 -0500

    (fix) Update styling for navbar; include new logo; reorder navbar components

--- client/app/components/mainNavbar.js
+++ client/app/components/mainNavbar.js
@@ -8,19 +8,18 @@ function Navbar(props) {
   return !props.isLoggedIn
     ? (
       <div>
-        <Link className={props.css.navItem} to='/signin' tabIndex='100'> Sign In </Link>
-        <Link className={props.css.navItem} to='/signup' tabIndex='110'> Sign Up </Link>
+        {/*<Link className={props.css.navItem} to='/signin' tabIndex='100'> Sign In </Link>
+                <Link className={props.css.navItem} to='/signup' tabIndex='110'> Sign Up </Link>*/}
         <Link className={props.css.navItem} to='/about' tabIndex='120'> What is oMdo? </Link>
       </div>
     )
     : (
       <div className={props.className}>
         {props.children}
-        <Link className={props.css.navItem} to='/twit' tabIndex='100'> Analyze Your Twitter Feed </Link>
-        <Link className={props.css.navItem} to='/meditations' tabIndex='110'> Daily Meditations </Link>
-        <Link className={props.css.navItem} to='/profile' tabIndex='120'> The Breakdown </Link>
-        <Link className={props.css.navItem} to='/about' tabIndex='120'> What is oMdo? </Link>
         <Link className={props.css.navItem} to='/signin' tabIndex='130' onClick={props.onSignOut}> Log Out </Link>
+        <Link className={props.css.navItem} to='/profile' tabIndex='120'> User Summary </Link>
+        <Link className={props.css.navItem} to='/meditations' tabIndex='110'> Daily Survey </Link>
+        <Link className={props.css.navItem} to='/twit' tabIndex='100'> Analyze Your Twitter Feed </Link>
       </div>
     )
 }
