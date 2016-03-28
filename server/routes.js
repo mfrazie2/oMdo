c02e3ebe67c762bb4fd21a8a24bc7f98fd32fbbf
John Soo <jsoo1@asu.edu>
2016-03-28 15:16:43 -0700

    (fix)Adds quote route to get quote

--- server/routes.js
+++ server/routes.js
@@ -2,10 +2,10 @@ var authRoutes = require('./auth/authRoutes');
 var surveyRoutes = require('./survey/surveyRoutes');
 var userRoutes = require('./user/userRoutes');
 var twitterRoutes = require('./twitter/twitterRoutes');
+var quoteRoutes = require('./quote/quoteRoutes');
 
 module.exports = function(app, express) {
-  // app.use('/auth', authRoutes);
-  // app.use('/survey', surveyRoutes);
   app.use('/user', userRoutes);
   app.use('/twitter', twitterRoutes);
+  app.use('/quote', quoteRoutes);
 };
