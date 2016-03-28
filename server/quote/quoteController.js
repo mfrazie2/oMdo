7dbe8f04f77565ca98cead33ce59c4a92b27b745
John Soo <jsoo1@asu.edu>
2016-03-28 15:32:21 -0700

    (fix)Adds default text for quote

--- server/quote/quoteController.js
+++ server/quote/quoteController.js
@@ -4,7 +4,17 @@ module.exports = {
   getQuote: function(req, res, next) {
     fs.readFile(__dirname + '/quote.json', 'utf8', function(err, data) {
       if (err) next(new Error('There was an error reading the quote file'));
-      res.json(data);
+      if (!data.length) res.send({
+          "success": {"total": 1},
+          "contents": {
+            "quote": "Please check back for a fresh chunk of wisdom.",
+            "author": "oMdo",
+            "id": "0",
+            "requested_category": "sanity",
+            "categories": []
+          }
+        })
+      res.json(JSON.parse(data));
     });
   }
 }
