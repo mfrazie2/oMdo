0910a74cb19752691cd11a842ce79b74ef2278db
chrislpatton <chrislpatton@gmail.com>
2016-03-28 20:38:22 -0400

    (fix) bug fix in generalNavBar

--- client/app/containers/homeContainer.js
+++ client/app/containers/homeContainer.js
@@ -5,24 +5,26 @@ var Home = require('../components/home');
 var connect = require('react-redux').connect;
 var bindActionCreators = require('redux').bindActionCreators;
 var homeActions = require('../actions/homeActions');
+var QuotePopUp = require('./modalContainer');
 
 var HomeContainer = React.createClass({
   componentWillMount: function() {
     this.props.actions.checkAuth();
+    this.props.actions.fetchQuote();
   },
   render: function() {
     var pathname = this.props.location.pathname;
     return (
       <Container>
-        <Greeting text='oMdo Welcomes You' username={this.props.username} location={this.props.location.pathname} />
-        <Home text='Home' />
+        <Greeting text='oMdo Welcomes You' username={this.props.username} />
+        <QuotePopUp text='Tips!' {...this.props} />
       </Container>
     )
   }
 });
 
 function mapStateToProps(state, ownProps) {
-  return Object.assign({}, state.authReducer);
+  return Object.assign({}, state.authReducer, state.homeReducer);
 };
 
 function mapDispatchToProps(dispatch) {
