af636f316a0bddb16ee2cd1b0e460544d20fa496
John Soo <jsoo1@asu.edu>
2016-03-29 10:47:17 -0700

    (fix)Fixes quote rendering

--- server/quote/quoteController.js
+++ server/quote/quoteController.js
@@ -13,10 +13,12 @@ var defaultRes = {
 module.exports = {
   getQuote: function(req, res, next) {
     fs.stat(__dirname + '/quote.json', function(err, stats) {
-      if (err) res.send(defaultRes);
+      if (err) {
+        res.send(defaultRes);
+        next();
+      }
       fs.readFile(__dirname + '/quote.json', 'utf8', function(err, data) {
-        if (err) res.send(defaultRes);
-        if (!data.length) res.send(defaultRes);
+        if (err || !data.length) res.send(defaultRes);
         res.json(JSON.parse(data));
       });
     });
