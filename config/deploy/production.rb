39c1c58a3ade7c89358a3e432957590dbaffc656
John Soo <jsoo1@asu.edu>
2016-03-30 12:27:22 -0700

    (test) Changes deployment server to bigger instance in production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,4 +5,4 @@ set :deploy_to, '/var/www/skynet'
 set :ssh_options, { :forward_agent => true }
 set :stage, :production
 
-server 'ec2-54-213-147-55.us-west-2.compute.amazonaws.com', user: 'ec2-user', roles: %w{web app}
+server 'ec2-54-213-68-115.us-west-2.compute.amazonaws.com', user: 'ec2-user', roles: %w{web app}
